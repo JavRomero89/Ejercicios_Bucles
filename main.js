1db6ace69d55ec2db02deb80465ad8d68ac4b918
JavRomero89 <javieromero23@gmail.com>
2023-06-29 16:20:06 +0200

    Advance

---
+++ main.js
@@ -0,0 +1,33 @@
+console.log("Ejercicio 1");
+for(let i=0;i<11;i++)
+{
+    console.log(i);
+}
+
+console.log("Ejercicio 2");
+for(let i=10;i>=0;i--)
+{
+    console.log(i);
+}
+
+console.log("Ejercicio 3");
+let number=9;
+for (num=0;num<11;num++)
+{
+    console.log(`${number} x ${num} =`,number*num);
+}
+
+console.log("Ejercicio 4");
+for (let i=0; i<11;i++)
+{console.log(`${i}   ${i*2}   ${i*3}`);
+
+console.log("Ejercicio 5");
+for(i = 0; i < 1; i++){
+    console.log(0+1+2+3+4+5+6+7+8+9+10);
+}
+
+console.log("Ejercicio 6");
+for(i = 0; i < 1; i++){
+    console.log(0*1*2*3*4*5*6*7*8*9*10);
+
+    
