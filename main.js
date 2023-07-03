8c5a1c562f8a5d3d1e438cd34b4f7148eb4c141d
JavRomero89 <javieromero23@gmail.com>
2023-07-03 17:08:28 +0200

    advance

--- main.js
+++ main.js
@@ -1,33 +1,59 @@
 console.log("Ejercicio 1");
-for(let i=0;i<11;i++)
-{
+for(let i=0;i<11;i++){
     console.log(i);
 }
 
 console.log("Ejercicio 2");
-for(let i=10;i>=0;i--)
-{
+for(let i=10;i>=0;i--){
     console.log(i);
 }
 
 console.log("Ejercicio 3");
-let number=9;
-for (num=0;num<11;num++)
-{
+let number=12;
+for (num=0;num<11;num++){
     console.log(`${number} x ${num} =`,number*num);
 }
 
 console.log("Ejercicio 4");
-for (let i=0; i<11;i++)
-{console.log(`${i}   ${i*2}   ${i*3}`);
-
-console.log("Ejercicio 5");
-for(i = 0; i < 1; i++){
-    console.log(0+1+2+3+4+5+6+7+8+9+10);
+for (let i=0; i<11;i++){
+    console.log(i,i**2,i**3);
 }
 
-console.log("Ejercicio 6");
-for(i = 0; i < 1; i++){
-    console.log(0*1*2*3*4*5*6*7*8*9*10);
+console.log("Ejercicio 5");
+let totalSum=0
+for(i = 0; i <= 10; i++){
+    totalSum+=i;
+}
+console.log(totalSum)
 
-    
+console.log("Ejercicio 6");
+let totalMul=0
+for(i = 0; i <= 10; i++){
+    totalMul*=i;
+}
+console.log(totalMul)
+
+
+
+console.log("Ejercicio 7");
+for(let i=0; i<101 ;i++){
+    if((i>=10 && i<=23) || i>=56 && i<=62){
+        console.log(i,"Rango");
+    }  
+    if (i%5===0 || i%7===0){
+        console.log(i,"Divisible");
+    }  
+}
+
+console.log("Ejercicio 8");
+
+
+
+
+
+console.log("Ejercicio 9");
+
+
+
+console.log("Ejercicio 10");
+
